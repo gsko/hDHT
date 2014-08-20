07d70dc465a988894d3e2f2cb18e56f8d10a45d0
Greg Skoczek <gsk067@gmail.com>
2014-08-19 22:42:55 -0500

    Added basic bshow QuickCheck property

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -5,21 +5,22 @@ import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
 import Test.HUnit
 import Test.QuickCheck
-import qualified Data.ByteString as BS
+import Text.Printf
+import qualified Data.ByteString.Char8 as BS
 
 import DHT.Bencode
+
+instance Show BVal where
+    show = (BS.unpack . bshow)
 
 main :: IO ()
 main = defaultMainWithOpts [
          testCase "bshow BInt" bshowTest
-       , testProperty "bshow BInt Length" True
+       , testProperty "bshow BInt Length" bShowIntProp 
        ] mempty
 
 bshowTest :: Assertion
 bshowTest = bshow (BInt 5) @?= "i5e"
 
-bshowProp :: BVal -> Bool
-bshowProp b@(BInt i) = (BS.length . bshow) b == 0
-
--- propListRevRevId :: [Int] -> Property
--- propListRevRevId xs = not (null xs) ==> reverse (reverse xs) == xs
+bShowIntProp :: Integer -> Property
+bShowIntProp i = property $ (BS.length . bshow . BInt) i == 2 + (length . show) i
