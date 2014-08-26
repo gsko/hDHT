d0b005a8b5dc0a04d64fed2016789e54e54246a1
Greg Skoczek <gsk067@gmail.com>
2014-08-25 22:15:58 -0500

    more bencode tests: this repeating of the test names is cumbersome..

--- test/BencodeTest.hs
+++ test/BencodeTest.hs
@@ -14,11 +14,13 @@ import qualified Data.Map.Strict as M
 import Text.Printf
 
 tests = [
-         testProperty "BInt encoding" bintEncode
-       , testProperty "BStr encoding" bstrEncode
-       , testCase "BList encoding" blistEncode
-       , testCase "BDict encoding" bdictEncode
-       , testCase "BInt decoding" bintDecode
+    testProperty "BInt encoding" bintEncode
+  , testProperty "BStr encoding" bstrEncode
+  , testCase "BList encoding" blistEncode
+  , testCase "BDict encoding" bdictEncode
+  , testProperty "BInt decoding" bintDecode
+  , testProperty "BStr decode" bstrDecode
+  , testCase "BStr decode empty" bstrEmpty
     ]
 
 bintEncode :: Integer -> Property 
@@ -30,12 +32,21 @@ bstrEncode s = property $ (BS.unpack . bshow . BStr) s ==
     (show . length) s ++ ":" ++ s
 
 blistEncode :: Assertion
-blistEncode = bshow (BList [BInt 5, BStr "hey"]) @?= "li5e3:heye"
+blistEncode = "li5e3:heye" @?=
+    (bshow . BList) [BInt 5, BStr "hey"]
 
 bdictEncode :: Assertion
-bdictEncode = bshow (BDict (M.fromList [(BStr "a", BInt 1)])) @?=
-    "d1:ai1ee"
+bdictEncode = "d1:ai1ee" @?=
+    (bshow . BDict . M.fromList) [(BStr "a", BInt 1)]
+    
+bintDecode :: Integer -> Property
+bintDecode n = case bdecode (printf "i%de" n) of
+    Right ([BInt n']) -> property $ n == n'
 
-bintDecode :: Assertion
-bintDecode = case bdecode "i5e" of
-    Right ([BInt i]) -> i @?= 5
+bstrDecode :: String -> Property
+bstrDecode s = case bdecode (printf "%d:%s" (length s) s) of
+    Right ([BStr s']) -> property $ s == s'
+
+bstrEmpty :: Assertion
+bstrEmpty = case bdecode "0:" of
+    Right ([BStr s]) -> s @?= ""
--- tests.sh
+++ tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -r dist
 cabal configure --enable-tests
 cabal build
 ./dist/build/reference/reference
