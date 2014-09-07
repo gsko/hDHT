f64daa556985206502c58f5ef643b39574015af2
Greg Skoczek <gsk067@gmail.com>
2014-09-06 23:16:59 -0500

    more testing; Node now has a Word160 as an id

--- DHT/Node.hs
+++ DHT/Node.hs
@@ -5,19 +5,20 @@ import Data.Bits
 
 -- Word160 : 160 bits = 20 bytes = 5 * 4 byte words = 5 Word32's
 data Word160 = Word160 !Word32 !Word32 !Word32 !Word32 !Word32
-    deriving (Show, Eq)
+    deriving (Show, Eq, Bounded)
 
 data Node = Node {
-    id :: Integer
+    id :: Word160
 } deriving (Show, Eq)
 
 dist :: Word160 -> Word160 -> Word160
-dist (Word160 a b c d e) (Word160 a' b' c' d' e') = Word160 (a+a') (b+b') (c+c') (d+d') (e+e')
+dist (Word160 a b c d e) (Word160 a' b' c' d' e') =
+    Word160 (a `xor` a') (b `xor` b') (c `xor` c') (d `xor` d') (e `xor` e')
 
 toInteger :: Word160 -> Integer
 toInteger (Word160 a b c d e) =
-    (fromIntegral $ a `shiftL` (4*4*8))
-  + (fromIntegral $ b `shiftL` (3*4*8))
-  + (fromIntegral $ c `shiftL` (2*4*8))
-  + (fromIntegral $ d `shiftL` (1*4*8))
-  + (fromIntegral e)
+    (Prelude.toInteger a) `shiftL` (4*32)
+  + (Prelude.toInteger b) `shiftL` (3*32)
+  + (Prelude.toInteger c) `shiftL` (2*32)
+  + (Prelude.toInteger d) `shiftL` (1*32)
+  + (Prelude.toInteger e) `shiftL` (0*32)
--- test/NodeTest.hs
+++ test/NodeTest.hs
@@ -1,6 +1,7 @@
 module NodeTest (tests) where
 
-import DHT.Node
+import DHT.Node as D
+import Data.Word
 
 import Test.Framework
 import Test.Framework.Providers.API
@@ -9,12 +10,51 @@ import Test.Framework.Providers.QuickCheck2
 import Test.HUnit
 import Test.QuickCheck
 
+instance Arbitrary D.Node where
+    arbitrary = arbitraryNode
+
+arbitraryNode :: Gen D.Node
+arbitraryNode = do
+    nodeID <- arbitraryWord160
+    return $ (Node nodeID)
+
+instance Arbitrary Word160 where
+    arbitrary = arbitraryWord160
+
+arbitraryWord160 :: Gen Word160
+arbitraryWord160 = do
+    let minW32 = fromIntegral $ (minBound :: Word32)
+        maxW32 = fromIntegral $ (maxBound :: Word32)
+    a <- choose (minW32, maxW32)
+    b <- choose (minW32, maxW32)
+    c <- choose (minW32, maxW32)
+    d <- choose (minW32, maxW32)
+    e <- choose (minW32, maxW32)
+    return $ (Word160 a b c d e)
+
 tests = TestGroup "NodeTest" [
-    testCase "test: node construction" (Node 5 @?= Node 5)
+    testCase "test: Word160 construction" (Word160 0 0 0 0 5 @?= Word160 0 0 0 0 5)
 
-  , testProperty "prop: node's self distance is zero"
-        (\n-> property $ (n :: Integer) == n)
+  , testCase "test: dist between same id is 0" (
+        let w = Word160 123 456 789 1011 1213 in
+            dist w w @?= Word160 0 0 0 0 0)
 
-  , testCase "test: " (DHT.Node.toInteger (Word160 0 0 0 0 0) @?= 0)
+  , testProperty "prop: Nodes with same ID have distance zero"
+        (\(Node n) (Node n') -> property $
+            n == n' ==> dist n n' == Word160 0 0 0 0 0)
 
+  , testCase "test: Word160->Integer 0 equality"
+        (D.toInteger (Word160 0 0 0 0 0) @?= 0)
+
+  , testCase "test: Word160->Integer 1 equality"
+        (D.toInteger (Word160 0 0 0 0 1) @?= 1)
+
+  , testCase "test: Word160->Integer 2^32 equality"
+        (D.toInteger (Word160 0 0 0 1 0) @?= 2^32)
+  
+  , testCase "test: Word160 can't be negative"
+        (D.toInteger (Word160 0 0 0 0 (-1)) < 0 @?= False)
+
+  , testCase "test: big Word160->Integer"
+        (D.toInteger (Word160 1 1 1 1 1) @?= 2^0 + 2^32 + 2^64 + 2^96 + 2^128)
     ]
