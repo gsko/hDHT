eb60fd7b009230597490110978a4cba274ce53df
Greg Skoczek <gsk067@gmail.com>
2014-09-17 21:20:22 -0500

    brought the udp_example code up to speed and moved it to become the Protocol file
    --HG--
    rename : Misc/udp_client.hs => src/DHT/Protocol.hs

--- src/DHT/Protocol.hs
+++ src/DHT/Protocol.hs
@@ -1,1 +1,23 @@
--- placeholder for Kademlia / MDHT protocol logic
+import Network.Socket
+import Data.List
+import Text.Printf
+
+pingQuery :: String
+--pingQuery = "d1:ad2:id20:1111111111111111111111:q4:ping1:t2:aa1:y1:qe"
+pingQuery = "d1:ad2:id20:abcdefghij0123456789e1:q4:ping1:t2:aa1:y1:qe"
+
+main :: IO()
+main = do
+    -- Prepare the socket
+    addrInfos <- getAddrInfo Nothing (Just "weezy.us") (Just "1337")
+    let addrInfo = head addrInfos
+    let serverAddr = addrAddress addrInfo
+    sock <- socket (addrFamily addrInfo) Datagram defaultProtocol
+     
+    -- Send the query
+    sent <- sendTo sock pingQuery serverAddr
+    printf "%s <-out- (%u bytes) \"%s\"\n" (show serverAddr) sent pingQuery 
+
+    -- Receive the response
+    (buf, bufLen, recvAddr) <- recvFrom sock 1024
+    printf "%s --in-> (%u bytes) %s\n" (show recvAddr) bufLen (show buf)
