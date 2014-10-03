4fee36ccf5b21795c6c3b44f47eeb6ae06c0c42a
Greg Skoczek <gsk067@gmail.com>
2014-10-02 21:17:20 -0500

    typo

--- DHT/KBucket.hs
+++ DHT/KBucket.hs
@@ -14,5 +14,6 @@ empty = KBucket [] (N.fromInteger 0) (N.fromInteger $ 2^160)
 
 offernode :: KBucket -> Node -> KBucket
 offernode k@(KBucket nodes minID maxID size) node@(Node nodeID)
+    | minID > maxID = error $ "KBucket invalid: minSize=" ++ show minID ++ " maxSize=" ++ show maxID
     | (nodeID < minID || nodeID > maxID) = k
     | (length nodes) < fromIntegral size = KBucket (node:nodes) minID maxID size
