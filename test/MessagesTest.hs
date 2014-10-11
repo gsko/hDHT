18b54ecd0b0a9ef48fdf1775f14f272c93ee248c
Greg Skoczek <gsk067@gmail.com>
2014-10-10 23:20:31 -0500

    encodeQuery FindNodeQ test case

--- test/MessagesTest.hs
+++ test/MessagesTest.hs
@@ -11,8 +11,16 @@ import Test.HUnit
 import Test.QuickCheck
 
 tests = TestGroup "MessagesTest" [
-    testCase "encodeQuery - Ping" test_encodeQuery_ping
+    testCase "encodeQuery - PingQ" test_encodeQuery_ping
+  , testCase "encodeQuery - FindNodeQ" test_encodeQuery_find_node
     ]
 
+nodeID,infohash :: N.Word160
+nodeID = N.fromInteger 5
+infohash = N.fromInteger 10
+
 test_encodeQuery_ping :: Assertion
-test_encodeQuery_ping = encodeQuery (PingQ $ N.fromInteger 5) @?= "d2:idi5e1:q4:pinge"
+test_encodeQuery_ping = "d2:idi5e1:q4:pinge" @=? encodeQuery (PingQ nodeID)
+
+test_encodeQuery_find_node :: Assertion
+test_encodeQuery_find_node = "d2:idi5e1:q9:find_node6:targeti10ee" @=? encodeQuery (FindNodeQ nodeID infohash)
