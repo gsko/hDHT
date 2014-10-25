2e55ef3c1d8989006661f36482f3c70a47c7d261
Greg Skoczek <gsk067@gmail.com>
2014-10-24 20:16:58 -0500

    whitespace removal

--- test/NodeTest.hs
+++ test/NodeTest.hs
@@ -34,34 +34,25 @@ arbitraryWord160 = do
 
 tests = TestGroup "NodeTest" [
     testCase "test: Word160 construction" (Word160 0 0 0 0 5 @?= Word160 0 0 0 0 5)
-
   , testCase "test: dist between same id is 0" (
         let w = Word160 123 456 789 1011 1213 in
             dist w w @?= Word160 0 0 0 0 0)
-
   , testCase "test: fromInteger 5+5*2^32" (
         D.fromInteger (5+5*2^32) @?= Word160 0 0 0 5 5)
-
   , testCase "test: maxBound of Word160" (
         let f :: Integer -> Integer -> Integer
             f acc n = acc + ((2^32 - 1) * 2^n) in
         (maxBound :: Word160) @?= (D.fromInteger $ foldl f 0 [128,96,64,32,0]))
-
   , testProperty "prop: Nodes with same ID have distance zero"
         (\(Node n) -> property $ dist n n == Word160 0 0 0 0 0)
-
   , testCase "test: Word160->Integer 0 equality"
         (D.toInteger (Word160 0 0 0 0 0) @?= 0)
-
   , testCase "test: Word160->Integer 1 equality"
         (D.toInteger (Word160 0 0 0 0 1) @?= 1)
-
   , testCase "test: Word160->Integer 2^32 equality"
         (D.toInteger (Word160 0 0 0 1 0) @?= 2^32)
-
   , testCase "test: Word160 can't be negative"
         (D.toInteger (Word160 0 0 0 0 (-1)) < 0 @?= False)
-
   , testCase "test: big Word160->Integer"
         (D.toInteger (Word160 1 1 1 1 1) @?= 2^0 + 2^32 + 2^64 + 2^96 + 2^128)
 
